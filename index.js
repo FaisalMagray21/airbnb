577d7a3b035f77f416a96c6b6893eb20eb95730e
FaisalMagray21 <136814501+FaisalMagray21@users.noreply.github.com>
2025-04-17 11:22:06 -0700

    homeadded

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -10,21 +12,12 @@ app.post('/',(req,res,next)=>{
     console.log('data received')
     next();
 })
-app.get('/',(req,res)=>{
-    res.send(`<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/output.css" rel="stylesheet">
 
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline text-center text-pink-500">
-    Hello world!
-  </h1>
-</body>
-</html>`);
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.get('/homeadded',(req,res)=>{
+    res.render('add-home');
 })
 app.listen(3000, () => {
     console.log('your serveris http://localhost:3000');
